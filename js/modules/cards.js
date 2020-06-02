bf3c017ae262149bd9276817b345d069e696a9a2
Olya <fedorovaolya0410@gmail.com>
2020-06-02 21:22:08 +0300

    es6 modules, webpack

---
+++ js/modules/cards.js
@@ -0,0 +1,66 @@
+function cards() {
+  class MenuCard {
+    constructor(src, alt, title, text, price, parentSelector, ...classes) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.text = text;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 27;
+      this.changeToUAN();
+      this.classes = classes;
+    }
+
+    changeToUAN() {
+      this.price *= this.transfer;
+    }
+    render() {
+      const element = document.createElement('div');
+
+      if (this.classes.length === 0) {
+        this.element = 'menu__item';
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach(className => element.classList.add(className));
+      }
+
+      element.innerHTML = `
+              <img src=${this.src} alt=${this.alt}>
+              <h3 class="menu__item-subtitle">${this.title}</h3>
+              <div class="menu__item-descr">${this.text}</div>
+              <div class="menu__item-divider"></div>
+              <div class="menu__item-price">
+                  <div class="menu__item-cost">Цена:</div>
+                  <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+              </div>
+          `;
+      this.parent.append(element);
+    }
+  }
+
+  const getRsource = async (url) => {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  };
+
+  axios.get('http://localhost:3000/menu')
+    .then(data => {
+      data.data.forEach(({
+        img,
+        alting,
+        title,
+        descr,
+        price
+      }) => {
+        new MenuCard(img, alting, title, descr, price, '.menu .container').render();
+      });
+    });
+}
+
+module.exports = cards;
